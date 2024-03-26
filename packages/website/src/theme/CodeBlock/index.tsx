b9fe8ec5c9ded62e578301321c37cf832dc5e01f
Timothee Guerin <tiguerin@microsoft.com>
2024-03-26 16:20:10 -0700

    Works

--- packages/website/src/theme/CodeBlock/index.tsx
+++ packages/website/src/theme/CodeBlock/index.tsx
@@ -1,4 +1,7 @@
 import { withTspPlayground } from "@site/src/components/tsp-tryit-codeblock/with-tsp-playground";
 import CodeBlockDocusaurus from "@theme-original/CodeBlock";
+import clsx from "clsx";
 
-export default withTspPlayground(CodeBlockDocusaurus);
+export default withTspPlayground((props) => (
+  <CodeBlockDocusaurus className={clsx(props.className, "shiki")} {...props} />
+));
